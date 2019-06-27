418bf3861126053e1e0473a3742e98d32a8e8f8e
Xin Li <xil222@ucsd.edu>
2019-06-26 22:37:33 -0700

    tree

---
+++ 98 Validate Binary Search Tree.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+//method 1, using bfs
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        Long minVal = Long.MIN_VALUE;
+        Long maxVal = Long.MAX_VALUE;
+        return isValid(root.left, minVal, root.val) && isValid(root.right, root.val, maxVal);
+    }
+
+    private boolean isValid(TreeNode node, long low, long high) {
+        if (node == null) {
+            return true;
+        }
+
+        if (node.val <= low || node.val >= high) {
+            return false;
+        }
+
+        return isValid(node.left, low, node.val) && isValid(node.right, node.val, high);
+    }
+
+}
+
+
+//method 2, using inorder traversal
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        TreeNode prev = null;
+
+        //two conditions to maintain for this while loop
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode node = stack.pop();
+                if (prev != null && prev.val >= node.val) {
+                    return false;
+                }
+                prev = node;
+                cur = node.right;
+            }
+        }
+        return true;
+    }
+}
+
+
