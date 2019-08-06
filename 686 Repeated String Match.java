a82bd8cb8231025bb40af36552d4843d3c86cbd3
Xin Li <xil222@ucsd.edu>
2019-08-06 00:26:44 -0700

    comments

--- 686 Repeated String Match.java
+++ 686 Repeated String Match.java
@@ -1,7 +1,14 @@
 class Solution {
     public int repeatedStringMatch(String A, String B) {
-        //if len(k * A) == len(B)
-        //check k * A, (k + 1) * A
+
+        //A has length m
+        //B has length n
+        //A has k replicates
+        //best case: n = mk
+        //worst case: n = (k - 1) * m + 1 + x(need another A)
+        //so when iterating to k which mk >= n but m(k-1) < n
+        //append one more
+
         int count = 0;
         StringBuilder sb = new StringBuilder();
         while (sb.length() < B.length()) {
